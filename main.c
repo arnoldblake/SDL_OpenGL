a170f68693bdc07d07dd9734e512de744e4caea4
Blake Arnold <barnold@blakearnold.me>
2021-06-25 12:38:06 -0400

    White triagle complete
    Updating file structure
    
    ignoring bin file
    
    testing gitignore
    
    found the issue with gitignore file

--- main.c
+++ main.c
@@ -1,5 +1,7 @@
+#include "glew.h"
 #include "SDL.h"
 #include "SDL_opengl.h"
+#include <stdio.h>
 #include <stdbool.h>
 
 int main (int argc, char *argv[]) {
@@ -13,12 +15,82 @@ int main (int argc, char *argv[]) {
     SDL_Window* window = SDL_CreateWindow("OpenGL", 100, 100, 800, 600, SDL_WINDOW_OPENGL);
     SDL_GLContext context = SDL_GL_CreateContext(window);
 
+    glewExperimental = true;
+    glewInit();
+
+    // Were ready to do things here
+    const char* vertexSource = "\
+    #version 150 core\n\
+    in vec2 position;\
+    void main() {\
+        gl_Position = vec4(position, 0.0, 1.0);\
+    }";
+
+    const char* fragmentSource = "\
+    #version 150 core\n\
+    out vec4 outColor;\
+    void main() {\
+        outColor = vec4(1.0, 1.0, 1.0, 1.0);\
+    }";
+
+    // Create Vertex Array Object
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+
+    // Create a Vertex Buffer Object and copy the vertex data to it
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    GLfloat vertices[] = {
+         0.0f,  0.5f,
+         0.5f, -0.5f,
+        -0.5f, -0.5f
+    };
+
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    // Create and compile the vertex shader
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexSource, NULL);
+    glCompileShader(vertexShader);
+
+    // Create and compile the fragment shader
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+    glCompileShader(fragmentShader);
+
+    // Link the vertex and fragment shader into a shader program
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glBindFragDataLocation(shaderProgram, 0, "outColor");
+    glLinkProgram(shaderProgram);
+    glUseProgram(shaderProgram);
+
+    // Specify the layout of the vertex data
+    GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+    glEnableVertexAttribArray(posAttrib);
+    glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 0, 0);
+
+
+    // I think we are stopping doing things here
+
     SDL_Event windowEvent;
     while (true) {
         if (SDL_PollEvent(&windowEvent))
         {
             if (windowEvent.type == SDL_QUIT) break;
         }
+        
+
+        // Clear the screen to black
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // Draw a triangle from the 3 vertices
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         SDL_GL_SwapWindow(window);
     }
